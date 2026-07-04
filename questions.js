4360a0218e7fa4ef2ae01dc0ee3a912d56519bc1
st3799 <exo502486@gmail.com>
2026-07-04 20:43:51 +0800

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,27 @@
+const questionBank = {
+
+limit:[
+{
+q:"lim sinx/x = ?",
+options:["0","1","∞","不存在"],
+a:1,
+explain:"基本极限 sinx/x=1"
+},
+{
+q:"lim(1-cosx)/x² = ?",
+options:["1","1/2","0","∞"],
+a:1,
+explain:"经典极限=1/2"
+}
+],
+
+derivative:[
+{
+q:"(x²)' = ?",
+options:["x","2x","x²","1"],
+a:1,
+explain:"幂函数求导"
+}
+]
+
+};
